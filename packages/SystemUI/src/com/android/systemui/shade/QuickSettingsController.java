b541ff7d4465e48cffdeaa4d3aa554e9a354936d
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-11-12 14:30:29 +0000

    [SQUASH] Feature: reticker [1/2]
    [someone5678]
    * Based on crDroid Android's cleaned A12 commit [1]
    * Forward port to Android 14 QPR1
    * Follow the Material You styles
    * Show text only if icon is null
    * Import SystemUIToast icon retrieving logic and adapt it
    * Use EXTRA_TITLE and EXTRA_TEXT as content
    
    This commit is also squashed of following commits:
    
    {1}
    * Properly update view for reTicker [2]
    * Use Tuner API for reTicker settings [3]
    * SystemUI: Fix Reticker not appearing for some apps [4]
    By: Pranav Vashi <neobuddy89@gmail.com>
    
    {2}
    * reTicker: Do not show when content/icon is null
    * reTicker: Cleanup
    By: cjh1249131356 <cjh1249131356@gmail.com>
    
    {3}
    * SystemUI: Fix reticker's notification background color
    * SystemUI: Fix reticker background based on app color exceptions
    (Cherry-picked from SystemUI: Refactor reticker feature [5])
    By: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Original commit:
    https://github.com/Descendant-XI/frameworks_base/commit/6ca0cff0da94d07a14ca8695b7bde5eaea9deef5
    
    Ref:
    [1] https://github.com/crdroidandroid/android_frameworks_base/commit/e44ede735e177d8297cfbfa8b22a21041c229ace
    [2] https://github.com/crdroidandroid/android_frameworks_base/commit/40ecee5e658b81f72eb26e6de8a4d1c664445df3
    [3] https://github.com/crdroidandroid/android_frameworks_base/commit/748adb415dd6ce33b3000f064019a42ec3b8905b
    [4] https://github.com/crdroidandroid/android_frameworks_base/commit/fd788046ae47f230a6f614adbcc0f2ae05903d94
    [5] https://github.com/minaripenguin/android_frameworks_base/commit/b8f53c0bacae67a823a1185f36c6926c6cdfca71
    
    Change-Id: I582d0484977f0e850d158e8645171e638fcc0c4c
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/shade/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/shade/QuickSettingsController.java
@@ -1053,6 +1053,7 @@ public class QuickSettingsController implements Dumpable {
         mShadeHeaderController.setShadeExpandedFraction(shadeExpandedFraction);
         mShadeHeaderController.setQsExpandedFraction(qsExpansionFraction);
         mShadeHeaderController.setQsVisible(mVisible);
+        mPanelViewControllerLazy.get().reTickerViewVisibility();
 
         // Update the light bar
         mLightBarController.setQsExpanded(mFullyExpanded);
