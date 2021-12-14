a8c9f7fb08bc48387cd157945278eacb5503159a
00day0 <therandomuser11@gmail.com>
2021-12-14 18:09:40 +0100

    SystemUI: Restart SystemUI on changing combined signal icons
    * Change takes effect after a restart of SystemUI, which needs to be done manually.
    
    Signed-off-by: Bartłomiej Rudecki <ejbtrd@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -247,6 +247,9 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
             resolver.registerContentObserver(
                     Settings.System.getUriFor(Settings.System.SHOW_FOURG_ICON), false,
                     this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(
+                    Settings.Secure.getUriFor(Settings.Secure.SHOW_COMBINED_STATUS_BAR_SIGNAL_ICONS), false,
+                    this, UserHandle.USER_ALL);
             updateSettings();
         }
 
@@ -254,7 +257,10 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
          *  @hide
          */
         @Override
-        public void onChange(boolean selfChange) {
+        public void onChange(boolean selfChange, Uri uri) {
+            if (uri.equals(Settings.Secure.getUriFor(Settings.Secure.SHOW_COMBINED_STATUS_BAR_SIGNAL_ICONS))) {
+                android.os.Process.killProcess(android.os.Process.myPid());
+            }
             updateSettings();
         }
     }
