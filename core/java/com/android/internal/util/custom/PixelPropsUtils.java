cd8d4a2f084a8e0694dd51680bf407897cf97183
jhenrique09 <jhsv09@gmail.com>
2023-02-23 16:44:35 -0300

    PixelPropsUtils: Don't declare propsToChange as static
    * Since that variable was static, it may hold props from undesired devices (like Pixel 7), use a new instance on every app to fix that.
    
    Change-Id: I7cf0836056a9cd5c9664ef0b203fc2a9bc5237f5
    (cherry picked from commit 4e31b9a013d1fc1fb4fef5fa5b21b4587fe9f56c)
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -38,7 +38,6 @@ public class PixelPropsUtils {
 
     private static final String SAMSUNG = "com.samsung.android.";
 
-    private static final Map<String, Object> propsToChange;
     private static final Map<String, Object> propsToChangePixel5;
     private static final Map<String, Object> propsToChangePixel7Pro;
     private static final Map<String, Object> propsToChangePixelXL;
@@ -156,7 +155,6 @@ public class PixelPropsUtils {
 
     static {
         propsToKeep = new HashMap<>();
-        propsToChange = new HashMap<>();
         propsToKeep.put("com.google.android.settings.intelligence", new ArrayList<>(Collections.singletonList("FINGERPRINT")));
         propsToChangePixel7Pro = new HashMap<>();
         propsToChangePixel7Pro.put("BRAND", "google");
@@ -209,6 +207,7 @@ public class PixelPropsUtils {
         if (Arrays.asList(packagesToKeep).contains(packageName)) {
             return;
         }
+        Map<String, Object> propsToChange = new HashMap<>();
         if (packageName.equals("com.google.android.gms")
             || packageName.toLowerCase().contains("androidx.test")
             || packageName.toLowerCase().equals("com.google.android.apps.restore")) {
